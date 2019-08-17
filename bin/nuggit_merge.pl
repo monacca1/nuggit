c61581590d66e9a2a8a03f4cf314178f103c429d
chris monaco <chris.monao@jhuapl.edu>
2019-08-17 01:58:34 +0000

    added more foundational stuff to nuggit_merge.pl

--- bin/nuggit_merge.pl
+++ bin/nuggit_merge.pl
@@ -48,8 +48,15 @@ use Cwd qw(getcwd);
 #    you will have to do this recursively in each repo, and then add and commit as you go up the tree.
 # ------------------------------------------------------------------------------------------------------------------
 
+sub merge_recursive();
+sub get_selected_branch_here();
+sub get_selected_branch($);
 
 my $root_dir;
+my $argc;
+my $source_branch = "";
+my $destination_branch;
+my $branch = "";
 
 $root_dir = `nuggit_find_root.pl`;
 chomp $root_dir;
@@ -60,6 +67,63 @@ if($root_dir eq "-1")
   exit();
 }
 
-print "nuggit_merge.pl - TO DO\n";
+print "nuggit_merge.pl\n";
+
+#print "changing directory to root: $root_dir\n";
+chdir $root_dir;
 
 
+$argc = @ARGV;  # get the number of arguments.
+if($argc == 1)
+{
+  $source_branch      = $ARGV[0];
+  $destination_branch = get_selected_branch_here();
+  
+  print "Source branch is: $source_branch\n";
+  print "destination branch is the current branch: $destination_branch\n";
+}
+else
+{
+  print "usage: nuggit_merge.pl <source_branch>\n";
+}
+
+
+
+
+merge_recursive();
+
+
+
+sub merge_recursive()
+{
+  print "merge recursive\n";
+}
+
+
+
+
+sub get_selected_branch_here()
+{
+  my $branches;
+  my $selected_branch;
+  
+#  print "Is branch selected here?\n";
+  
+  # execute git branch
+  $branches = `git branch`;
+
+  $selected_branch = get_selected_branch($branches);  
+}
+
+sub get_selected_branch($)
+{
+  my $root_repo_branches = $_[0];
+  my $selected_branch;
+
+  $selected_branch = $root_repo_branches;
+  $selected_branch =~ m/\*.*/;
+  $selected_branch = $&;
+  $selected_branch =~ s/\* //;  
+  
+  return $selected_branch;
+}
