41d1667d3820a255745cc43095289f1b0c8dd4f0
chris monaco <chris.monao@jhuapl.edu>
2019-08-23 03:55:37 +0000

    check if the .gitmodules file exists, if not, suppress output

--- bin/list_submodules.sh
+++ bin/list_submodules.sh
@@ -1,4 +1,8 @@
-#sed '/path/d' .gitmodules | sed '/url/d' | sed 's/\[submodule \"//' | sed 's/\"\]//'
+#!/bin/bash
 
+#sed '/path/d' .gitmodules | sed '/url/d' | sed 's/\[submodule \"//' | sed 's/\"\]//'
 #grep submodule .gitmodules | sed 's/\[submodule \"//' | sed 's/\"\]//'
+
+if test -f ".gitmodules" ; then
 grep path .gitmodules | sed 's/path//' | sed 's/\=//' | sed 's/ //g'  | sed 's/\t//g'
+fi
