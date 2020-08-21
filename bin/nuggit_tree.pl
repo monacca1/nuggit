b19d2eeb52f0e3ff4c37e17e9f41039f06063e51
Chris Monaco <chris.monaco@jhuapl.edu>
2020-08-21 11:14:03 -0400

    added the start of a nuggit tree command - to help show the status of a nuggit repo

---
+++ bin/nuggit_tree.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+#*******************************************************************************
+##                           COPYRIGHT NOTICE
+##      (c) 2019 The Johns Hopkins University Applied Physics Laboratory
+##                         All rights reserved.
+##
+##  Permission is hereby granted, free of charge, to any person obtaining a 
+##  copy of this software and associated documentation files (the "Software"), 
+##  to deal in the Software without restriction, including without limitation 
+##  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+##  and/or sell copies of the Software, and to permit persons to whom the 
+##  Software is furnished to do so, subject to the following conditions:
+## 
+##     The above copyright notice and this permission notice shall be included 
+##     in all copies or substantial portions of the Software.
+##
+##  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+##  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+##  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+##  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+##  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+##  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+##  DEALINGS IN THE SOFTWARE.
+##
+#*******************************************************************************/
+
+use strict;
+use warnings;
+use v5.10;
+use Getopt::Long;
+Getopt::Long::Configure ("bundling"); # ie: to allow "status -ad"
+use Pod::Usage;
+use Cwd qw(getcwd);
+use FindBin;
+use lib $FindBin::Bin.'/../lib'; # Add local lib to path
+use Data::Dumper; # Debug and --dump option
+use Git::Nuggit;
+
+print `git ls-tree -r HEAD | grep commit`
